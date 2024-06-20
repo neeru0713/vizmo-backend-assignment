4ce192932d9a7ce86f5654b4fe4b6642632c469f
neeru0713 <neerurani1307@gmail.com>
2024-06-20 19:17:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const authRoutes = require("./routes/authRoutes.js");
+// const blogRoutes = require("./routes/blogRoutes.js");
+
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+// app.use("/api/auth", authRoutes);
+// app.use("/api/blogs", blogRoutes);
+
+
+
+mongoose
+  .connect(
+    "mongodb+srv://neerurani1307:%40Neeru1307@neerucluster.z4krrc9.mongodb.net/vizmobackendassignment?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  
